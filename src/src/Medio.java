29ed73b1d49d6cc25434af2f210dacc72bab3b34
David Machado <machado.david190@gmail.com>
2021-06-07 15:21:09 +0100

    rename percecao_de_jogo para recuperacao

--- src/src/Medio.java
+++ src/src/Medio.java
@@ -2,25 +2,24 @@ import java.util.Date;
 import java.util.Map;
 
 public class Medio extends Futebolista {
+    private int recuperacao;
 
-    private int percecao_de_jogo;
-
-    public Medio(int idAtleta, String nome, Date data_de_nascimento, int velocidade, int resistencia, int destreza, int impulsao, int cabeceamento, int remate, int passe, Map<Integer, Equipa> historial, int percecao_de_jogo) {
+    public Medio(int idAtleta, String nome, Date data_de_nascimento, int velocidade, int resistencia, int destreza, int impulsao, int cabeceamento, int remate, int passe, Map<Integer, Equipa> historial, int recuperacao) {
         super(idAtleta, nome, data_de_nascimento, velocidade, resistencia, destreza, impulsao, cabeceamento, remate, passe, historial);
-        this.percecao_de_jogo = percecao_de_jogo;
+        this.recuperacao = recuperacao;
     }
 
     public Medio(Medio m) {
         super(m.getIdAtleta(), m.getNome(), m.getData_de_nascimento(), m.getVelocidade(), m.getResistencia(), m.getDestreza(), m.getImpulsao(), m.getCabeceamento(), m.getRemate(), m.getPasse(), m.getHistorial());
-        this.percecao_de_jogo = m.getPercecao_de_jogo();
+        this.recuperacao = m.getRecuperacao();
     }
 
-    public int getPercecao_de_jogo() {
-        return percecao_de_jogo;
+    public int getRecuperacao() {
+        return recuperacao;
     }
 
-    public void setPercecao_de_jogo(int percecao_de_jogo) {
-        this.percecao_de_jogo = percecao_de_jogo;
+    public void setRecuperacao(int recuperacao) {
+        this.recuperacao = recuperacao;
     }
 
     public Medio clone() {
@@ -28,6 +27,6 @@ public class Medio extends Futebolista {
     }
 
     public int getOverall() {
-        return this.percecao_de_jogo;
+        return this.recuperacao;
     }
 }
