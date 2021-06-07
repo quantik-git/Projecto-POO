e40cbb19c755fa031fcf4a56a1ee0f20fe416fbb
David Machado <machado.david190@gmail.com>
2021-06-07 15:20:17 +0100

    Add classe lateral

---
+++ src/src/Lateral.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+import java.util.Map;
+
+public class Lateral extends Futebolista{
+    private int cruzamento;
+
+    public Lateral(int idAtleta, String nome, Date data_de_nascimento, int velocidade, int resistencia, int destreza, int impulsao, int cabeceamento, int remate, int passe, Map<Integer, Equipa> historial, int cruzamento) {
+        super(idAtleta, nome, data_de_nascimento, velocidade, resistencia, destreza, impulsao, cabeceamento, remate, passe, historial);
+        this.cruzamento = cruzamento;
+    }
+
+    public Lateral(Lateral l) {
+        super(l.getIdAtleta(), l.getNome(), l.getData_de_nascimento(), l.getVelocidade(), l.getResistencia(), l.getDestreza(), l.getImpulsao(), l.getCabeceamento(), l.getRemate(), l.getPasse(), l.getHistorial());
+        this.cruzamento = l.getCruzamento();
+    }
+
+    public int getCruzamento() {
+        return cruzamento;
+    }
+
+    public void setCruzamento(int cruzamento) {
+        this.cruzamento = cruzamento;
+    }
+
+    @Override
+    public Futebolista clone() {
+        return new Lateral(this);
+    }
+
+    @Override
+    public int getOverall() {
+        return 0;
+    }
+}
